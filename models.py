8ec2bf5a062a94a15c47af5c2b90e195af0ecd86
Ken <37133791+ken-11@users.noreply.github.com>
2018-05-09 11:22:34 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,6 @@
 #-*- coding: UTF-8 -*-
 from django.db import models
 from django import forms
-# Create your models here.
-# class Userinfo(forms.From):... 可以使用内置的表单验证渠道
-
-
 
 # 用户信息
 class UserInfo(models.Model):
@@ -12,8 +8,6 @@ class UserInfo(models.Model):
     uname = models.CharField('用户名', max_length=50, db_column='uname', null=False)
     upassword = models.CharField('密码', max_length=200, db_column='upassword', null=False)
     isban = models.BooleanField('禁用', default=False)
-    # isactive = models.BooleanField('激活', default = False)
-    # ucellphone = models.CharField('手机', default ='086')
 
     def __str__(self):
         return self.uname
@@ -24,8 +18,6 @@ class UserInfo(models.Model):
     class Meta():
         db_table = 'users'
 
-
-#
 class Address(models.Model):
     aname = models.CharField('姓名', max_length=30, db_column='aname', null=False)
     address = models.CharField('收站点', max_length=150, db_column='address', null=False)
